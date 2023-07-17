0f532c8e7f4363c1f7fd0f523c84130890d16b49
florin-b <gflorinb@yahoo.com>
2023-07-17 14:19:24 +0300

    Update Utils.java

--- src/poligoane/utils/Utils.java
+++ src/poligoane/utils/Utils.java
@@ -1,5 +1,5 @@
 package poligoane.utils;
 
 public class Utils {
-	public static String GOOGLE_PAYED_KEY = "AIzaSyDkYyLbUm5o3gnlwiH2l8A7M-WvWnazqDI";
+	public static String GOOGLE_PAYED_KEY = "";
 }
